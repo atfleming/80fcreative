88dd8de11f224603383a328284599688c75c69d9
atfleming <atfleming@gmail.com>
2025-10-16 13:23:10 -0500

    updated about page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { AnimatedVideoSection } from './components/AnimatedVideoSection';
 import { About } from './components/About';
 import { Services } from './components/Services';
 import { SideScrollGallery, Project } from './components/SideScrollGallery';
-import { About } from './components/About'
 import { Services } from './components/Services'
 // import { VideoPortfolio } from './components/VideoPortfolio'
 // import { Development } from './components/Development'
