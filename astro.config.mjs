7262ccc7d246bb344d6d7f87f2cdecbe3759310a
atfleming <atfleming@gmail.com>
2025-12-17 10:40:14 -0600

    chore: cache bust for Vercel rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+// Cache bust: 2025-12-17 10:37:00 UTC
 export default defineConfig({
   site: 'https://example.com',
   server: { host: true },
